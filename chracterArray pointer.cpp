3d51a100af33203a41e1578edf281da8c56890af
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2022-12-27 10:50:48 +0530

    Create chracterArray pointer.cpp

---
+++ chracterArray pointer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+   
+   
+    int arr[5] = {1,2,3,4,5};
+    char ch[6] = "abcde";
+
+
+    cout << arr << endl;
+    //attention here
+    cout << ch << endl;
+    char *c = &ch[0];
+    //prints entire string
+    cout << c << endl;
+    cout<< "address "<<&ch<<endl;
+
+    char temp = 'h';
+    char *p = &temp;
+
+    cout << p << endl;
+
+
+
+  return 0;
+}
