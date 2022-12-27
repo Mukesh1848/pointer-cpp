c9295930437d283d517aa2e7e9703c2676823075
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2022-12-27 11:41:44 +0530

    Create Mcqs pointer.cpp

---
+++ Mcqs pointer.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main() {
+
+//   int first = 8;
+//     int second = 18;
+//     int *ptr = &second;
+//     *ptr = 9;
+//     cout  << first << " " << second << endl;
+
+
+    // int first = 6;
+    // int *p = &first;
+    // int *q = p;
+    // (*q)++;
+    // cout << first  << endl;
+    
+    // int first = 8;
+    // int *p = &first;
+    // cout << (*p)++ << " ";
+    // cout << first << endl;
+    
+    // ERROR
+    // int *p = 0;
+    // int first = 110;
+    // *p = first;
+    // cout << *p << endl;
+    
+    // int first = 8;
+    // int second = 11;
+    // int *third = &second; 
+    // first = *third;
+    // *third = *third + 2;
+    // cout  << first << "  " << second << endl;
+    
+    // float f = 12.5;
+    // float p = 21.5;
+    // float* ptr = &f;
+    // (*ptr)++;
+    // *ptr = p;
+    // cout << *ptr << " " << f << " " << p << endl;
+    
+    //  int arr[5];
+    // int *ptr;
+    // cout << sizeof(arr) << " " << sizeof(ptr) << endl;
+    
+    // int arr[4] = {11, 21, 13, 14};
+    // cout << *(arr) << " " << *(arr+1) << endl;
+
+    // int arr[6] = {11, 12, 31};
+    // cout << arr << " " << &arr << endl;
+
+    // int arr[6] = {11, 21, 13};
+    // cout<<arr<<endl;
+    // cout << (arr + 1) << endl;
+    
+    // int arr[6] = {11, 21, 31};
+    // int *p = arr;
+    // cout << p[2] << endl;
+
+    // int arr[] = {11, 12, 13, 14, 15};
+    // cout << *(arr) << " " << *(arr + 3);
+
+    // int arr[] = {11, 21, 31, 41};
+    // int *ptr = arr++;
+    // cout << *ptr << endl;
+    
+    return 0;
+}
