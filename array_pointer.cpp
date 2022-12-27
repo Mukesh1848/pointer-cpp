52bbde086fd3b59b5761ee45f58213f9b2ccdb0c
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2022-12-27 10:45:10 +0530

    Create array_pointer.cpp

---
+++ array_pointer.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+   
+ int arr[5] = {2,4,6,8,0};
+ 
+ 
+ /*
+ 
+     // size of arr and pointer
+     cout<<sizeof(arr)<<endl;     // arr store 4* of no. of array [20] 
+     cout<<sizeof(&arr);        // pointer store 8 byte
+     
+ 
+    //  address of 0 Indexed of arr
+     cout<<arr<<endl;
+     cout<<&arr<<endl;
+     cout<<&arr[0]<<endl;
+     
+     // value of at 0 indexed 
+      cout<<arr[0]<<endl;
+      cout<<*arr<<endl;
+  
+  
+    cout << "4th " << *arr << endl;
+    cout << "5th " << *arr + 1 << endl;
+    cout << "6th " << *(arr + 1) << endl;
+    cout << "7th " << *(arr) + 1 << endl;
+    cout << "8th " << arr[2] << endl;
+    cout << "9th " << *(arr+2) << endl;
+  
+  
+  cout<<&arr<<endl;
+  cout<<&arr+1<<endl;
+  cout<<&arr[1]<<endl;
+  cout<<&arr[4]<<endl;
+  
+
+    int temp[10] = {1,2};
+   cout << sizeof(temp) << endl;  // 40
+   cout << " 1st " <<  sizeof(*temp) << endl;  // 4
+   cout << " 2nd " <<  sizeof(&temp) << endl;   // 8
+   
+   */
+   
+   
+  return 0;
+}
